da78525782dfa9da0d2fe7b9481b6e75633821db
Brent Brimhall <brent.brimhall@gmail.com>
2015-12-18 21:23:25 -0700

    Lucky 13: Reducer composition with arrays

--- client_src/todo.js
+++ client_src/todo.js
@@ -1,24 +1,43 @@
 import { createStore } from "redux";
 
-const todos = (state = [], action) => {
+/**
+ * Reducer composition:  Different reducers specify how different parts of the
+ * state tree are updated in response to actions.
+ */
+
+// reducer for a single item
+const todo = (state, action) => {
     switch (action.type) {
         case "ADD_TODO":
-            return [...state, {
+            return {
                 id: action.id,
                 text: action.text,
                 completed: false
-            }];
+            };
         case "TOGGLE_TODO":
-            return state.map(todo => {
-                if (todo.id !== action.id) {
-                    return todo;
-                } else {
-                    return {
-                        ...todo,
-                        completed: !todo.completed
-                    };
-                }
-            });
+            if (state.id !== action.id) {
+                return state;
+            } else {
+                return {
+                    ...state,
+                    completed: !state.completed
+                };
+            }
+        default:
+            return state;
+    }
+};
+
+// reducer for a list of items
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case "TOGGLE_TODO":
+            return state.map(t => todo(t, action));
         default:
             return state;
     }
