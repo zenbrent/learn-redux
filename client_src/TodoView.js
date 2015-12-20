5e72717b5de0b7da65267f2b937816d5068f1069
Brent Brimhall <brent.brimhall@gmail.com>
2015-12-20 11:45:32 -0700

    18: React Todo List Example (Toggling a Todo)

--- client_src/TodoView.js
+++ client_src/TodoView.js
@@ -7,26 +7,39 @@ let nextTodoId = 0;
 class TodoApp extends Component {
     render () {
         return (
-            <div>
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                store.dispatch({
+                    type: "ADD_TODO",
+                    text: this.input.value,
+                    id: nextTodoId++
+                });
+                this.input.value = "";
+            }}>
                 <input ref={node => {
                     this.input = node;
                 }} />
-                <button onClick={() => {
-                    store.dispatch({
-                        type: "ADD_TODO",
-                        text: this.input.value,
-                        id: nextTodoId++
-                    });
-                    this.input.value = "";
-                }}>Add Todo</button>
+                <input type="submit" value="Add Todo" />
                 <ul>
                     {this.props.todos.map(todo =>
-                        <li key={todo.id}>
+                        <li key={todo.id}
+                            onClick={() => {
+                                store.dispatch({
+                                    type: "TOGGLE_TODO",
+                                    id: todo.id
+                                });
+                            }}
+                            style={{
+                                textDecoration: 
+                                    todo.completed ?
+                                    "line-through" :
+                                    "none"
+                            }}>
                             {todo.text}
                         </li>
                     )}
                 </ul>
-            </div>
+            </form>
         );
     }
 }
