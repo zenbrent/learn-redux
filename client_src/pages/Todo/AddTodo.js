5de44c47181aa037fb4c6d95c03b4fbaddb25e1a
Brent Brimhall <brent.brimhall@gmail.com>
2015-12-21 08:42:42 -0700

    Extracted AddTodo and Footer into their own files.

---
+++ client_src/pages/Todo/AddTodo.js
@@ -0,0 +1,32 @@
+import { connect } from "react-redux";
+import React from "react";
+
+let nextTodoId = 0;
+
+// 2nd argument in a functional component is the context.
+let AddTodo = ({ dispatch }) => {
+    let input;
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            dispatch({
+                type: "ADD_TODO",
+                id: nextTodoId++,
+                text: input.value
+            })
+            input.value = "";
+        }}>
+            <input ref={node => { input = node; }} />
+            <input type="submit" value="Add Todo" />
+        </form>
+    );
+}
+AddTodo = connect()(AddTodo);
+
+// AddTodo = connect(
+//     // there are no props that depend on the current state. Null tells it to not subscribe to the store.
+//     null,
+//     null // defaults to: dispatch => ({ dispatch })
+// )(AddTodo);
+
+export { AddTodo };
--- client_src/pages/Todo/index.js
+++ client_src/pages/Todo/index.js
@@ -1,51 +1,11 @@
 import React, {Component} from "react";
 
 import { todoApp } from "../../todo";
+import { AddTodo } from "./AddTodo";
 import { VisibleTodoList } from "./Todo";
-import { FilterLink } from "./FilterLink";
+import { Footer } from "./Footer";
 import { createStore } from "redux";
-import { Provider, connect } from "react-redux";
-
-let nextTodoId = 0;
-
-const Footer = () => (
-    <p>
-        Show: {" "}
-        <FilterLink filter="SHOW_ALL">All</FilterLink> {", "}
-        <FilterLink filter="SHOW_ACTIVE">Active</FilterLink> {", "}
-        <FilterLink filter="SHOW_COMPLETED">Completed</FilterLink>
-    </p>
-)
-Footer.contextTypes = {
-    store: React.PropTypes.object
-};
-
-// 2nd argument in a functional component is the context.
-let AddTodo = ({ dispatch }) => {
-    let input;
-    return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            dispatch({
-                type: "ADD_TODO",
-                id: nextTodoId++,
-                text: input.value
-            })
-            input.value = "";
-        }}>
-            <input ref={node => { input = node; }} />
-            <input type="submit" value="Add Todo" />
-        </form>
-    );
-}
-AddTodo = connect()(AddTodo);
-
-// AddTodo = connect(
-//     // there are no props that depend on the current state. Null tells it to not subscribe to the store.
-//     null,
-//     null // defaults to: dispatch => ({ dispatch })
-// )(AddTodo);
-
+import { Provider } from "react-redux";
 
 // General approach:
 // Extract presentational components
